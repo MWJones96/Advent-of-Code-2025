fbaee8cb1c8c7619b49bf404867ca124550be818
Matthew Jones <mwjones0896@gmail.com>
2025-12-04 22:47:03 +0000

    AI generated Day4 boilerplate - amazing

---
+++ src/day4/day4.rs
@@ -0,0 +1,18 @@
+fn part1() {
+    let _input = include_str!("input.txt");
+    // TODO: implement Part 1
+    println!("(Part 1) not implemented yet");
+}
+
+fn part2() {
+    let _input = include_str!("input.txt");
+    // TODO: implement Part 2
+    println!("(Part 2) not implemented yet");
+}
+
+pub fn day4() {
+    println!("---- DAY 4 ----");
+    part1();
+    part2();
+    println!("");
+}
