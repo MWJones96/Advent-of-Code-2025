32507410028de387ebe443394ea43e3988536eb4
Matthew Jones <mwjones0896@gmail.com>
2025-12-04 23:56:23 +0000

    Boilerplate for remaining days

---
+++ src/day12/day12.rs
@@ -0,0 +1,18 @@
+fn part1() {
+    let _input = include_str!("input.txt");
+    // TODO: implement Part 1
+    println!("(Part 1) not implemented yet");
+}
+
+fn part2() {
+    let _input = include_str!("input.txt");
+    // TODO: implement Part 2
+    println!("(Part 2) not implemented yet");
+}
+
+pub fn day12() {
+    println!("---- DAY 12 ----");
+    part1();
+    part2();
+    println!("");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 mod day1;
+mod day10;
+mod day11;
+mod day12;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
 
 fn main() {
     day1::day1::day1();
     day2::day2::day2();
     day3::day3::day3();
     day4::day4::day4();
+    //day5::day5::day5();
+    //day6::day6::day6();
+    //day7::day7::day7();
+    //day8::day8::day8();
+    //day9::day9::day9();
+    //day10::day10::day10();
+    //day11::day11::day11();
+    //day12::day12::day12();
 }
