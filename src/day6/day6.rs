32c20cc0df93726e1bdb91b77cd2554a55057d63
Matthew Jones <mwjones0896@gmail.com>
2025-12-06 23:20:57 +0000

    Day 6 - Part 2

--- src/day6/day6.rs
+++ src/day6/day6.rs
@@ -1,3 +1,6 @@
+use core::num;
+use std::ffi::c_char;
+
 fn part1() {
     let input = include_str!("input.txt");
     let input: Vec<&str> = input.split("\n").collect();
@@ -39,8 +42,53 @@ fn part1() {
 }
 
 fn part2() {
-    let _input = include_str!("input.txt");
-    println!("(Part 2) not implemented yet");
+    let input = include_str!("input.txt");
+    let input: Vec<&str> = input.split("\n").collect();
+    let input: Vec<Vec<char>> = input.iter().map(|s| (*s).chars().collect()).collect();
+
+    let mut current_op: Option<(char, u64)> = Option::None;
+    let mut sum = 0;
+    for col in 0..input[0].len() {
+        let op = input[input.len() - 1][col];
+        if op != ' ' {
+            current_op = match op {
+                '+' => Some(('+', 0)),
+                '*' => Some(('*', 1)),
+                _ => {
+                    panic!("Invalid symbol")
+                }
+            }
+        }
+
+        let mut num: u64 = 0;
+        for row in 0..input.len() - 1 {
+            let c = input[row][col];
+            if c.is_digit(10) {
+                num = num * 10 + (c as u8 - b'0') as u64;
+            }
+        }
+
+        if num == 0 {
+            sum += current_op.unwrap().1;
+            continue;
+        }
+
+        match &mut current_op {
+            Some(('+', curr)) => {
+                *curr += num;
+            }
+            Some(('*', curr)) => {
+                *curr *= num;
+            }
+            _ => {
+                panic!("Invalid operator")
+            }
+        }
+    }
+
+    sum += current_op.unwrap().1;
+
+    println!("(Part 2) Sum of answers: {}", sum);
 }
 
 pub fn day6() {
