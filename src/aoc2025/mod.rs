b85c902dc78b46890e6b9eb258e8f6e21c8ec231
Matthew Jones <mwjones0896@gmail.com>
2025-12-07 22:21:43 +0000

    Run specific days

--- src/aoc2025/mod.rs
+++ src/aoc2025/mod.rs
@@ -11,18 +11,27 @@ pub mod day7;
 pub mod day8;
 pub mod day9;
 
+pub fn run_day(day: u32) {
+    match day {
+        1 => day1::day1::day1(),
+        2 => day2::day2::day2(),
+        3 => day3::day3::day3(),
+        4 => day4::day4::day4(),
+        5 => day5::day5::day5(),
+        6 => day6::day6::day6(),
+        7 => day7::day7::day7(),
+        8 => day8::day8::day8(),
+        9 => day9::day9::day9(),
+        10 => day10::day10::day10(),
+        11 => day11::day11::day11(),
+        12 => day12::day12::day12(),
+        _ => println!("Invalid day: {}. Expected 1-12.", day),
+    }
+}
+
 pub fn run_all() {
     println!("**** Advent of Code 2025 ****");
-    day1::day1::day1();
-    day2::day2::day2();
-    day3::day3::day3();
-    day4::day4::day4();
-    day5::day5::day5();
-    day6::day6::day6();
-    day7::day7::day7();
-    day8::day8::day8();
-    day9::day9::day9();
-    day10::day10::day10();
-    day11::day11::day11();
-    day12::day12::day12();
+    for day in 1..=12 {
+        run_day(day);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,70 @@ mod aoc2025;
 
 #[allow(dead_code, unused_variables, unused_imports)]
 fn print_usage() {
-    println!("Usage: cargo run -- <year>");
-    println!("  year: 2024 | 2025 | all");
+    println!("Usage:");
+    println!("  cargo run -- <year>                  Run all days for the year");
+    println!("  cargo run -- <year> <day>           Run specific day for the year");
+    println!("  cargo run -- all                     Run all days for all years");
+    println!();
+    println!("Examples:");
+    println!("  cargo run -- 2025                    Run all 2025 days");
+    println!("  cargo run -- 2025 7                  Run 2025 day 7");
+    println!("  cargo run -- 2024 15                 Run 2024 day 15");
+    println!("  cargo run -- all                     Run all years");
 }
 
 fn main() {
-    let arg = std::env::args().nth(1);
-    match arg.as_deref() {
-        Some("2024") => {
-            aoc2024::run_all();
+    let mut args = std::env::args();
+    args.next(); // Skip program name
+
+    let year_arg = match args.next() {
+        Some(y) => y,
+        None => {
+            print_usage();
+            return;
         }
-        Some("2025") => {
-            aoc2025::run_all();
+    };
+
+    let day_arg = args.next();
+
+    match year_arg.as_str() {
+        "2024" => {
+            if let Some(day_str) = day_arg {
+                match day_str.parse::<u32>() {
+                    Ok(day) => aoc2024::run_day(day),
+                    Err(_) => {
+                        eprintln!("Invalid day: '{}'", day_str);
+                        print_usage();
+                    }
+                }
+            } else {
+                aoc2024::run_all();
+            }
         }
-        Some("all") => {
-            aoc2024::run_all();
-            aoc2025::run_all();
+        "2025" => {
+            if let Some(day_str) = day_arg {
+                match day_str.parse::<u32>() {
+                    Ok(day) => aoc2025::run_day(day),
+                    Err(_) => {
+                        eprintln!("Invalid day: '{}'", day_str);
+                        print_usage();
+                    }
+                }
+            } else {
+                aoc2025::run_all();
+            }
+        }
+        "all" => {
+            if day_arg.is_some() {
+                eprintln!("Cannot specify day with 'all' year");
+                print_usage();
+            } else {
+                aoc2024::run_all();
+                aoc2025::run_all();
+            }
         }
         _ => {
+            eprintln!("Invalid year: '{}'", year_arg);
             print_usage();
         }
     }
