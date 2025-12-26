f509ffc7159cb65ac8b1fb8884ffa2169b825289
Matthew Jones <mwjones0896@gmail.com>
2025-12-26 23:01:10 +0000

    Day 11 - Part 1

--- src/aoc2025/day11/day11.rs
+++ src/aoc2025/day11/day11.rs
@@ -1,7 +1,44 @@
+use std::collections::HashMap;
+
+fn dp<'a>(
+    start: &'a str,
+    seen: &mut HashMap<&'a str, u32>,
+    graph: &HashMap<&'a str, Vec<&'a str>>,
+) -> u32 {
+    if start == "out" {
+        return 1;
+    }
+
+    if seen.contains_key(start) {
+        return *seen.get(start).unwrap();
+    }
+
+    let sum: Vec<u32> = graph
+        .get(start)
+        .unwrap()
+        .iter()
+        .map(|n| dp(*n, seen, graph))
+        .collect();
+    let sum: u32 = sum.iter().sum();
+    seen.insert(start, sum);
+
+    sum
+}
+
 fn part1() {
-    let _input = include_str!("input.txt");
-    // TODO: implement Part 1
-    println!("(Part 1) not implemented yet");
+    let input = include_str!("input.txt");
+    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in input.lines() {
+        let (node, next) = line.split_once(':').unwrap();
+        let next = next.trim();
+        let next: Vec<_> = next.split_whitespace().collect();
+        graph.insert(node, next);
+    }
+    let mut seen = HashMap::new();
+    println!(
+        "(Part 1) Number of paths to output: {}",
+        dp("you", &mut seen, &graph)
+    );
 }
 
 fn part2() {
