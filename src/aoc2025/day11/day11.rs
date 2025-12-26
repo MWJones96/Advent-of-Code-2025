bf5bf4c8f45f1c0663107fb4b6fd33e4dd6d313f
Matthew Jones <mwjones0896@gmail.com>
2025-12-26 23:34:39 +0000

    Day 11 - Part 2

--- src/aoc2025/day11/day11.rs
+++ src/aoc2025/day11/day11.rs
@@ -25,6 +25,42 @@ fn dp<'a>(
     sum
 }
 
+fn dp2<'a>(
+    start: &'a str,
+    need_fft: bool,
+    need_dac: bool,
+    seen: &mut HashMap<(&'a str, bool, bool), u64>,
+    graph: &HashMap<&'a str, Vec<&'a str>>,
+) -> u64 {
+    if start == "out" {
+        if !need_fft && !need_dac {
+            return 1;
+        }
+        return 0;
+    }
+    if seen.contains_key(&(start, need_fft, need_dac)) {
+        return *seen.get(&(start, need_fft, need_dac)).unwrap();
+    }
+    let sum: Vec<u64> = graph
+        .get(start)
+        .unwrap()
+        .iter()
+        .map(|n| {
+            dp2(
+                *n,
+                if start == "fft" { false } else { need_fft },
+                if start == "dac" { false } else { need_dac },
+                seen,
+                graph,
+            )
+        })
+        .collect();
+    let sum: u64 = sum.iter().sum();
+    seen.insert((start, need_fft, need_dac), sum);
+
+    sum
+}
+
 fn part1() {
     let input = include_str!("input.txt");
     let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
@@ -42,9 +78,20 @@ fn part1() {
 }
 
 fn part2() {
-    let _input = include_str!("input.txt");
-    // TODO: implement Part 2
-    println!("(Part 2) not implemented yet");
+    let input = include_str!("input.txt");
+    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in input.lines() {
+        let (node, next) = line.split_once(':').unwrap();
+        let next = next.trim();
+        let next: Vec<_> = next.split_whitespace().collect();
+        graph.insert(node, next);
+    }
+    let mut seen: HashMap<(&str, bool, bool), u64> = HashMap::new();
+
+    println!(
+        "(Part 2) FFT and DAC paths: {}",
+        dp2("svr", true, true, &mut seen, &graph)
+    );
 }
 
 pub fn day11() {
