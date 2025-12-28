f86090b632fa1954d930d7f2a284856a29e738fd
Matthew Jones <mwjones0896@gmail.com>
2025-12-28 18:14:53 +0000

    2024 Day 1 - Part 1

--- src/aoc2024/day1/day1.rs
+++ src/aoc2024/day1/day1.rs
@@ -1,11 +1,29 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 fn part1() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 1) Part 1 not implemented yet");
+    let input = include_str!("input.txt");
+    let mut total_distance: u64 = 0;
+    let mut l_heap = BinaryHeap::new();
+    let mut r_heap = BinaryHeap::new();
+    for line in input.lines() {
+        if let Some(v) = line.split_once(" ") {
+            let l = v.0.trim().parse::<u32>().unwrap();
+            let r = v.1.trim().parse::<u32>().unwrap();
+            l_heap.push(Reverse(l));
+            r_heap.push(Reverse(r));
+        }
+    }
+
+    while let (Some(l), Some(r)) = (l_heap.pop(), r_heap.pop()) {
+        total_distance += l.0.abs_diff(r.0) as u64;
+    }
+    println!("(Day 1) Total distance between lists: {}", total_distance);
 }
 
 fn part2() {
     let _input = include_str!("input.txt");
-    println!("(2024 Day 1) Part 2 not implemented yet");
+    println!("(Day 1)");
 }
 
 pub fn day1() {
