d36916b3705ec1991d260fc130c1d9db61b75677
Matthew Jones <mwjones0896@gmail.com>
2025-12-28 23:35:15 +0000

    Day 2 - Part 2

--- src/aoc2024/day2/day2.rs
+++ src/aoc2024/day2/day2.rs
@@ -1,40 +1,71 @@
-fn part1() {
-    enum Dir {
-        Inc,
-        Dec,
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Dir {
+    Inc,
+    Dec,
+}
+
+fn is_valid_report(report: &Vec<u32>, skips: i32, prev: u32, idx: usize, dir: Dir) -> bool {
+    if skips < 0 {
+        return false;
     }
+
+    if idx == report.len() {
+        return true;
+    }
+
+    let cont = match dir {
+        Dir::Inc => report[idx] > prev && report[idx] - prev <= 3,
+        Dir::Dec => report[idx] < prev && prev - report[idx] <= 3,
+    };
+
+    if !cont {
+        return is_valid_report(report, skips - 1, prev, idx + 1, dir);
+    }
+
+    let mut res = is_valid_report(report, skips, report[idx], idx + 1, dir);
+
+    if idx == 0 {
+        res = res
+            || match dir {
+                Dir::Inc => is_valid_report(report, skips - 1, report[idx + 1] - 1, idx + 1, dir),
+                Dir::Dec => is_valid_report(report, skips - 1, report[idx + 1] + 1, idx + 1, dir),
+            };
+    } else {
+        res = res || is_valid_report(report, skips - 1, prev, idx + 1, dir);
+    }
+
+    return res;
+}
+
+fn part1() {
     let input = include_str!("input.txt");
     let mut safe = 0;
 
-    'lines: for line in input.lines() {
-        let v: Vec<_> = line.split(" ").collect();
-        let v: Vec<u32> = v.iter().map(|n| n.parse::<u32>().unwrap()).collect();
-        let dir = if v[1] > v[0] { Dir::Inc } else { Dir::Dec };
-        for i in 1..v.len() {
-            match dir {
-                Dir::Inc => {
-                    if v[i] <= v[i - 1] {
-                        continue 'lines;
-                    }
-                }
-                Dir::Dec => {
-                    if v[i] >= v[i - 1] {
-                        continue 'lines;
-                    }
-                }
-            }
-            if v[i].abs_diff(v[i - 1]) > 3 {
-                continue 'lines;
-            }
+    for line in input.lines() {
+        let report: Vec<_> = line.split(" ").collect();
+        let report: Vec<u32> = report.iter().map(|n| n.parse::<u32>().unwrap()).collect();
+        if is_valid_report(&report, 0, report[0] - 1, 0, Dir::Inc)
+            || is_valid_report(&report, 0, report[0] + 1, 0, Dir::Dec)
+        {
+            safe += 1;
         }
-        safe += 1;
     }
     println!("(Part 1) Number of safe reports: {}", safe);
 }
 
 fn part2() {
     let input = include_str!("input.txt");
-    println!("(Part 2) Part 2 not implemented yet");
+    let mut safe = 0;
+    for line in input.lines() {
+        let report: Vec<_> = line.split(" ").collect();
+        let report: Vec<u32> = report.iter().map(|n| n.parse::<u32>().unwrap()).collect();
+        if is_valid_report(&report, 1, report[0] - 1, 0, Dir::Inc)
+            || is_valid_report(&report, 1, report[0] + 1, 0, Dir::Dec)
+        {
+            safe += 1;
+        }
+    }
+    println!("(Part 2) Number of safe reports: {}", safe);
 }
 
 pub fn day2() {
