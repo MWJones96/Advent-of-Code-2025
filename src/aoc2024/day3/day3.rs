90897ea33f9e5508bfd7a7186e6a886ad57dcf63
Matthew Jones <mwjones0896@gmail.com>
2025-12-28 23:46:02 +0000

    Day 3 - Part 1

--- src/aoc2024/day3/day3.rs
+++ src/aoc2024/day3/day3.rs
@@ -1,15 +1,28 @@
+use regex::Regex;
+
 fn part1() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 3) Part 1 not implemented yet");
+    let input = include_str!("input.txt");
+    let re = Regex::new(r"mul\(\d+\,\d+\)").unwrap();
+    let mut sum: u64 = 0;
+    for line in input.lines() {
+        for capture in re.find_iter(line) {
+            let str = &capture.as_str()[4..capture.len() - 1];
+            let (x, y) = str.split_once(",").unwrap();
+
+            sum += x.parse::<u64>().unwrap() * y.parse::<u64>().unwrap();
+        }
+    }
+
+    println!("(Part 1) Sum of multiplications: {}", sum);
 }
 
 fn part2() {
     let _input = include_str!("input.txt");
-    println!("(2024 Day 3) Part 2 not implemented yet");
+    println!("(Part 2) Part 2 not implemented yet");
 }
 
 pub fn day3() {
-    println!("---- 2024 DAY 3 ----");
+    println!("---- DAY 3 ----");
     part1();
     part2();
     println!("");
