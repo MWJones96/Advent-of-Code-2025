8bb2462a11f58ee6aa76468edb7d4ff49011e89a
Matthew Jones <mwjones0896@gmail.com>
2025-12-28 18:26:41 +0000

    2024 Day 1 - Part 2

--- src/aoc2024/day1/day1.rs
+++ src/aoc2024/day1/day1.rs
@@ -1,5 +1,5 @@
 use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 
 fn part1() {
     let input = include_str!("input.txt");
@@ -18,12 +18,29 @@ fn part1() {
     while let (Some(l), Some(r)) = (l_heap.pop(), r_heap.pop()) {
         total_distance += l.0.abs_diff(r.0) as u64;
     }
-    println!("(Day 1) Total distance between lists: {}", total_distance);
+    println!("(Part 1) Total distance between lists: {}", total_distance);
 }
 
 fn part2() {
-    let _input = include_str!("input.txt");
-    println!("(Day 1)");
+    let input = include_str!("input.txt");
+    let mut right_freq = HashMap::new();
+    let mut sim_score: u64 = 0;
+    let mut left_nums = vec![];
+    for line in input.lines() {
+        if let Some(v) = line.split_once(" ") {
+            let l = v.0.trim().parse::<u32>().unwrap();
+            left_nums.push(l);
+            let r = v.1.trim().parse::<u32>().unwrap();
+            let val = right_freq.entry(r).or_insert(0);
+            *val += 1;
+        }
+    }
+
+    for num in left_nums {
+        sim_score += num as u64 * right_freq.get(&num).unwrap_or(&0);
+    }
+
+    println!("(Part 2) Similarity Score: {}", sim_score);
 }
 
 pub fn day1() {
