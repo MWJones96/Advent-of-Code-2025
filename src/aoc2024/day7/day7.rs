0244889d2b01ec8c329076320a2e336d43e3d808
Matthew Jones <mwjones0896@gmail.com>
2025-12-31 17:06:54 +0000

    Day 7 - Part 1

--- src/aoc2024/day7/day7.rs
+++ src/aoc2024/day7/day7.rs
@@ -1,6 +1,29 @@
+fn is_valid(nums: &Vec<u64>, idx: usize, int_num: u64, target_num: u64) -> bool {
+    if idx == nums.len() {
+        return int_num == target_num;
+    }
+    if int_num > target_num {
+        return false;
+    }
+
+    is_valid(nums, idx + 1, int_num * nums[idx], target_num)
+        || is_valid(nums, idx + 1, int_num + nums[idx], target_num)
+}
+
 fn part1() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 7) Part 1 not implemented yet");
+    let input = include_str!("input.txt");
+    let mut sum: u64 = 0;
+    for line in input.lines() {
+        let (target, nums) = line.split_once(":").unwrap();
+        let target = target.parse::<u64>().unwrap();
+        let nums = nums.trim();
+        let nums: Vec<u64> = nums.split(" ").map(|n| n.parse::<u64>().unwrap()).collect();
+        if is_valid(&nums, 1, nums[0], target) {
+            sum += target;
+        }
+    }
+
+    println!("(Part 1) Total calibration result: {}", sum);
 }
 
 fn part2() {
