51cc7531c1bf69e806365245054943f6f35b3a00
Matthew Jones <mwjones0896@gmail.com>
2026-01-03 00:09:24 +0000

    Day 11 - Part 2

--- src/aoc2024/day11/day11.rs
+++ src/aoc2024/day11/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn part1() {
     let input = include_str!("input.txt");
     let mut stones: Vec<u64> = input
@@ -22,12 +24,48 @@ fn part1() {
 
         stones = new_stones;
     }
-    println!("(Part 1) Number of stones: {}", stones.len());
+    println!(
+        "(Part 1) Number of stones after 25 blinks: {}",
+        stones.len()
+    );
 }
 
 fn part2() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 11) Part 2 not implemented yet");
+    let input = include_str!("input.txt");
+    let stones: Vec<u64> = input
+        .split(" ")
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect();
+    let mut freq: HashMap<u64, u64> = HashMap::new();
+    for stone in stones {
+        *freq.entry(stone).or_default() += 1;
+    }
+    for _ in 0..75 {
+        let mut new_freq: HashMap<u64, u64> = HashMap::new();
+        for (num, occurrences) in freq {
+            let stone_str = num.to_string();
+            if num == 0 {
+                *new_freq.entry(1).or_default() += occurrences;
+            } else if stone_str.len() % 2 == 0 {
+                let first_half = &stone_str[0..stone_str.len() / 2];
+                let second_half = &stone_str[stone_str.len() / 2..];
+                *new_freq
+                    .entry(first_half.parse::<u64>().unwrap())
+                    .or_default() += occurrences;
+                *new_freq
+                    .entry(second_half.parse::<u64>().unwrap())
+                    .or_default() += occurrences;
+            } else {
+                *new_freq.entry(num * 2024).or_default() += occurrences;
+            }
+        }
+        freq = new_freq;
+    }
+
+    println!(
+        "(Part 2) Number of stones after 75 blinks: {}",
+        freq.values().sum::<u64>()
+    );
 }
 
 pub fn day11() {
