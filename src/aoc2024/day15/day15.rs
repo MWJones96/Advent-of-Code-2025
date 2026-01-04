b95b34190e72906690d91ae60167904be3aa303d
Matthew Jones <mwjones0896@gmail.com>
2026-01-04 23:39:25 +0000

    Day 15 - Part 2

--- src/aoc2024/day15/day15.rs
+++ src/aoc2024/day15/day15.rs
@@ -9,6 +9,19 @@ enum GridElement {
     RightBox,
 }
 
+#[derive(Copy, Clone)]
+enum Polarity {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Copy, Clone)]
+struct Direction {
+    row_diff: i8,
+    col_diff: i8,
+    polarity: Polarity,
+}
+
 impl fmt::Display for GridElement {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -21,11 +34,81 @@ impl fmt::Display for GridElement {
     }
 }
 
+fn can_push(grid: &mut Vec<Vec<GridElement>>, pos: (isize, isize), dir: Direction) -> bool {
+    match grid[pos.0 as usize][pos.1 as usize] {
+        GridElement::Wall => false,
+        GridElement::Space => true,
+        GridElement::LeftBox | GridElement::RightBox => {
+            let (row, col) = pos;
+
+            match dir.polarity {
+                Polarity::Horizontal => {
+                    //L-R
+                    let next_c = col + dir.col_diff as isize;
+                    let can_push = can_push(grid, (row, next_c), dir);
+                    can_push
+                }
+                Polarity::Vertical => {
+                    //T-B
+                    let offset = if grid[row as usize][col as usize] == GridElement::LeftBox {
+                        1
+                    } else {
+                        -1
+                    };
+                    let next_row = row + dir.row_diff as isize;
+                    let can_push = can_push(grid, (next_row, col), dir)
+                        && can_push(grid, (next_row, col + offset), dir);
+                    can_push
+                }
+            }
+        }
+        GridElement::Box => panic!("Full Box not in Part 2"),
+    }
+}
+
+fn push2(grid: &mut Vec<Vec<GridElement>>, pos: (isize, isize), dir: Direction) {
+    match grid[pos.0 as usize][pos.1 as usize] {
+        GridElement::Wall => {}
+        GridElement::Space => {}
+        GridElement::LeftBox | GridElement::RightBox => {
+            let (row, col) = pos;
+
+            match dir.polarity {
+                Polarity::Horizontal => {
+                    //L-R
+                    let next_c = col + dir.col_diff as isize;
+                    push2(grid, (row, next_c), dir);
+                    grid[row as usize][next_c as usize] = grid[row as usize][col as usize];
+                    grid[row as usize][col as usize] = GridElement::Space;
+                }
+                Polarity::Vertical => {
+                    //T-B
+                    let offset = if grid[row as usize][col as usize] == GridElement::LeftBox {
+                        1
+                    } else {
+                        -1
+                    };
+                    let next_row = row + dir.row_diff as isize;
+                    push2(grid, (next_row, col), dir);
+                    push2(grid, (next_row, col + offset), dir);
+                    grid[next_row as usize][col as usize] = grid[row as usize][col as usize];
+                    grid[next_row as usize][(col + offset) as usize] =
+                        grid[row as usize][(col + offset) as usize];
+
+                    grid[row as usize][col as usize] = GridElement::Space;
+                    grid[row as usize][(col + offset) as usize] = GridElement::Space;
+                }
+            }
+        }
+        GridElement::Box => panic!("Full Box not in Part 2"),
+    }
+}
+
 fn push(grid: &mut Vec<Vec<GridElement>>, pos: (isize, isize), dir: (isize, isize)) -> bool {
     match grid[pos.0 as usize][pos.1 as usize] {
         GridElement::Wall => false,
         GridElement::Space => true,
-        _ => {
+        GridElement::Box => {
             let can_push = push(grid, (pos.0 + dir.0, pos.1 + dir.1), dir);
             if can_push {
                 grid[(pos.0 + dir.0) as usize][(pos.1 + dir.1) as usize] =
@@ -34,6 +117,7 @@ fn push(grid: &mut Vec<Vec<GridElement>>, pos: (isize, isize), dir: (isize, isiz
             }
             can_push
         }
+        _ => panic!("Left and Right Box not in Part 1"),
     }
 }
 
@@ -116,7 +200,7 @@ fn part2() {
                         v.push(GridElement::Space);
                     }
                     '@' => {
-                        pos = (row as isize, col as isize);
+                        pos = (row as isize, (col * 2) as isize);
                         v.push(GridElement::Space);
                         v.push(GridElement::Space);
                     }
@@ -125,17 +209,58 @@ fn part2() {
             }
             grid.push(v);
         } else {
+            for ch in line.chars() {
+                let (row, col) = pos;
+                let dir = match ch {
+                    '^' => Direction {
+                        row_diff: -1,
+                        col_diff: 0,
+                        polarity: Polarity::Vertical,
+                    },
+                    '<' => Direction {
+                        row_diff: 0,
+                        col_diff: -1,
+                        polarity: Polarity::Horizontal,
+                    },
+                    '>' => Direction {
+                        row_diff: 0,
+                        col_diff: 1,
+                        polarity: Polarity::Horizontal,
+                    },
+                    'v' => Direction {
+                        row_diff: 1,
+                        col_diff: 0,
+                        polarity: Polarity::Vertical,
+                    },
+                    _ => panic!("Invalid character"),
+                };
+
+                if can_push(
+                    &mut grid,
+                    (row + dir.row_diff as isize, col + dir.col_diff as isize),
+                    dir,
+                ) {
+                    push2(
+                        &mut grid,
+                        (row + dir.row_diff as isize, col + dir.col_diff as isize),
+                        dir,
+                    );
+                    pos = (row + dir.row_diff as isize, col + dir.col_diff as isize);
+                }
+            }
         }
     }
 
+    let mut sum: u64 = 0;
     for (row, line) in grid.iter().enumerate() {
         for (col, element) in line.iter().enumerate() {
-            print!("{}", element);
+            if *element == GridElement::LeftBox {
+                sum += 100 * row as u64 + col as u64;
+            }
         }
-        println!();
     }
 
-    println!("(Part 2) Part 2 not implemented yet");
+    println!("(Part 2) Sum of GPS co-ordinates: {}", sum);
 }
 
 pub fn day15() {
