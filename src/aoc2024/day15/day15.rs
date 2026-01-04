128b9213705d4d9424536133d432790391bcca9f
Matthew Jones <mwjones0896@gmail.com>
2026-01-04 20:23:08 +0000

    Day 15 - Part 1 Refactor

--- src/aoc2024/day15/day15.rs
+++ src/aoc2024/day15/day15.rs
@@ -1,10 +1,12 @@
 use std::fmt;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum GridElement {
     Wall,
     Space,
     Box,
+    LeftBox,
+    RightBox,
 }
 
 impl fmt::Display for GridElement {
@@ -13,6 +15,24 @@ impl fmt::Display for GridElement {
             GridElement::Wall => write!(f, "#"),
             GridElement::Space => write!(f, "."),
             GridElement::Box => write!(f, "O"),
+            GridElement::LeftBox => write!(f, "["),
+            GridElement::RightBox => write!(f, "]"),
+        }
+    }
+}
+
+fn push(grid: &mut Vec<Vec<GridElement>>, pos: (isize, isize), dir: (isize, isize)) -> bool {
+    match grid[pos.0 as usize][pos.1 as usize] {
+        GridElement::Wall => false,
+        GridElement::Space => true,
+        _ => {
+            let can_push = push(grid, (pos.0 + dir.0, pos.1 + dir.1), dir);
+            if can_push {
+                grid[(pos.0 + dir.0) as usize][(pos.1 + dir.1) as usize] =
+                    grid[pos.0 as usize][pos.1 as usize];
+                grid[pos.0 as usize][pos.1 as usize] = GridElement::Space;
+            }
+            can_push
         }
     }
 }
@@ -52,25 +72,8 @@ fn part1() {
                     _ => panic!("Invalid character"),
                 };
 
-                match grid[(pos.0 + dir.0) as usize][(pos.1 + dir.1) as usize] {
-                    GridElement::Wall => {}
-                    GridElement::Space => pos = (pos.0 + dir.0, pos.1 + dir.1),
-                    GridElement::Box => {
-                        let mut lookahead = (pos.0 + dir.0, pos.1 + dir.1);
-
-                        while grid[lookahead.0 as usize][lookahead.1 as usize] == GridElement::Box {
-                            lookahead = (lookahead.0 + dir.0, lookahead.1 + dir.1);
-                        }
-                        match grid[lookahead.0 as usize][lookahead.1 as usize] {
-                            GridElement::Wall => {}
-                            GridElement::Space => {
-                                pos = (pos.0 + dir.0, pos.1 + dir.1);
-                                grid[pos.0 as usize][pos.1 as usize] = GridElement::Space;
-                                grid[lookahead.0 as usize][lookahead.1 as usize] = GridElement::Box;
-                            }
-                            GridElement::Box => panic!("Error"),
-                        }
-                    }
+                if push(&mut grid, (pos.0 + dir.0, pos.1 + dir.1), dir) {
+                    pos = (pos.0 + dir.0, pos.1 + dir.1)
                 }
             }
         }
@@ -87,8 +90,52 @@ fn part1() {
 }
 
 fn part2() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 15) Part 2 not implemented yet");
+    let input = include_str!("input.txt");
+    let mut is_reading_grid = true;
+    let mut grid: Vec<Vec<GridElement>> = vec![];
+    let mut pos: (isize, isize) = (0, 0);
+    for (row, line) in input.lines().enumerate() {
+        if line == "" {
+            is_reading_grid = false;
+        }
+
+        if is_reading_grid {
+            let mut v: Vec<GridElement> = vec![];
+            for (col, ch) in line.chars().enumerate() {
+                match ch {
+                    '#' => {
+                        v.push(GridElement::Wall);
+                        v.push(GridElement::Wall);
+                    }
+                    'O' => {
+                        v.push(GridElement::LeftBox);
+                        v.push(GridElement::RightBox);
+                    }
+                    '.' => {
+                        v.push(GridElement::Space);
+                        v.push(GridElement::Space);
+                    }
+                    '@' => {
+                        pos = (row as isize, col as isize);
+                        v.push(GridElement::Space);
+                        v.push(GridElement::Space);
+                    }
+                    _ => panic!("Invalid character"),
+                }
+            }
+            grid.push(v);
+        } else {
+        }
+    }
+
+    for (row, line) in grid.iter().enumerate() {
+        for (col, element) in line.iter().enumerate() {
+            print!("{}", element);
+        }
+        println!();
+    }
+
+    println!("(Part 2) Part 2 not implemented yet");
 }
 
 pub fn day15() {
