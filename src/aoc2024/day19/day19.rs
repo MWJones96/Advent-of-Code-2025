155de893e1613540e92bb4f025e962a52516b258
Matthew Jones <mwjones0896@gmail.com>
2026-01-29 17:26:46 +0000

    Day 19 - Part 1

--- src/aoc2024/day19/day19.rs
+++ src/aoc2024/day19/day19.rs
@@ -1,6 +1,34 @@
+use std::collections::HashSet;
+
 fn part1() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 19) Part 1 not implemented yet");
+    fn dfs(line: &str, start: usize, options: &HashSet<&str>) -> bool {
+        if start == line.len() {
+            return true;
+        }
+
+        for end in start..line.len() {
+            if options.contains(&line[start..=end]) && dfs(line, end + 1, options) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    let input = include_str!("input.txt");
+
+    let lines = input.lines().collect::<Vec<&str>>();
+    let mut options = lines[0]
+        .split(",")
+        .map(|w| w.trim())
+        .collect::<HashSet<&str>>();
+    let mut cnt = 0;
+    for line in &lines[2..] {
+        if dfs(*line, 0, &mut options) {
+            cnt += 1;
+        }
+    }
+    println!("(Part 1) Possible designs: {}", cnt);
 }
 
 fn part2() {
@@ -9,7 +37,7 @@ fn part2() {
 }
 
 pub fn day19() {
-    println!("---- 2024 DAY 19 ----");
+    println!("---- DAY 19 ----");
     part1();
     part2();
     println!("");
