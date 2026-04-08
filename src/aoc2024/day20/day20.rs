d54d3be424ca28fe409e883d1d9673509f936372
Matthew Jones <mwjones0896@gmail.com>
2026-04-08 12:41:03 +0100

    Day 20 - Part 1

--- src/aoc2024/day20/day20.rs
+++ src/aoc2024/day20/day20.rs
@@ -1,6 +1,73 @@
+use std::collections::HashMap;
+
+fn dfs(
+    grid: &Vec<&str>,
+    seen: &mut HashMap<(usize, usize), usize>,
+    row: usize,
+    col: usize,
+    depth: usize,
+) {
+    if grid[row].chars().nth(col).unwrap() == '#' {
+        return;
+    }
+    if seen.contains_key(&(row, col)) {
+        return;
+    }
+
+    seen.insert((row, col), depth);
+
+    dfs(grid, seen, row + 1, col, depth + 1);
+    dfs(grid, seen, row - 1, col, depth + 1);
+    dfs(grid, seen, row, col + 1, depth + 1);
+    dfs(grid, seen, row, col - 1, depth + 1);
+}
+
 fn part1() {
-    let _input = include_str!("input.txt");
-    println!("(2024 Day 20) Part 1 not implemented yet");
+    let input = include_str!("input.txt");
+    let grid: Vec<&str> = input.split("\n").collect();
+    let mut skips: u64 = 0;
+    let mut seen: HashMap<(usize, usize), usize> = HashMap::new();
+    for (row_i, row) in grid.iter().enumerate() {
+        for (col_i, col) in row.chars().enumerate() {
+            if col == 'E' {
+                dfs(&grid, &mut seen, row_i, col_i, 0);
+                break;
+            }
+        }
+    }
+
+    for (row, col) in seen.keys() {
+        let dist_to_end = *seen.get(&(*row, *col)).unwrap();
+        if *row >= 2 && seen.contains_key(&(row - 2, *col)) {
+            let skip_dist = 2 + seen.get(&(row - 2, *col)).unwrap();
+            let timesave = dist_to_end as i64 - skip_dist as i64;
+            if timesave >= 100 {
+                skips += 1;
+            }
+        }
+        if seen.contains_key(&(row + 2, *col)) {
+            let skip_dist = 2 + seen.get(&(row + 2, *col)).unwrap();
+            let timesave = dist_to_end as i64 - skip_dist as i64;
+            if timesave >= 100 {
+                skips += 1;
+            }
+        }
+        if *col >= 2 && seen.contains_key(&(*row, col - 2)) {
+            let skip_dist = 2 + seen.get(&(*row, col - 2)).unwrap();
+            let timesave = dist_to_end as i64 - skip_dist as i64;
+            if timesave >= 100 {
+                skips += 1;
+            }
+        }
+        if seen.contains_key(&(*row, col + 2)) {
+            let skip_dist = 2 + seen.get(&(*row, col + 2)).unwrap();
+            let timesave = dist_to_end as i64 - skip_dist as i64;
+            if timesave >= 100 {
+                skips += 1;
+            }
+        }
+    }
+    println!("(Part 1) Cheats saving >= 100ps: {}", skips);
 }
 
 fn part2() {
@@ -9,7 +76,7 @@ fn part2() {
 }
 
 pub fn day20() {
-    println!("---- 2024 DAY 20 ----");
+    println!("---- DAY 20 ----");
     part1();
     part2();
     println!("");
